a1135f1ba34a8ce47a6d57f11bfc9b8535fd5549
Md Enamul Haque <enamul807849@gmail.com>
2025-07-29 11:20:20 +0600

    update

--- src/components/employees/EmployeeForm.jsx
+++ src/components/employees/EmployeeForm.jsx
@@ -194,13 +194,13 @@ export const EmployeeForm = ({
             required
           >
             <option value="">Select Holiday</option>
-            <option value="1">Saturday</option>
-            <option value="2">Sunday</option>
-            <option value="3">Monday</option>
-            <option value="4">Tuesday</option>
-            <option value="5">Wednesday</option>
-            <option value="6">Thursday</option>
-            <option value="7">Friday</option>
+            <option value="6">Saturday</option>
+            <option value="7">Sunday</option>
+            <option value="1">Monday</option>
+            <option value="2">Tuesday</option>
+            <option value="3">Wednesday</option>
+            <option value="4">Thursday</option>
+            <option value="5">Friday</option>
           </select>
         </div>
 
